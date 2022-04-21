8598cea344c865e7e9682b7b5736e41ef918d8c5
Rio Foster <friotious@gmail.com>
2022-04-20 17:32:07 -0700

    Add class imports

--- test/Hotel-test.js
+++ test/Hotel-test.js
@@ -2,8 +2,8 @@ import chai from 'chai';
 const expect = chai.expect;
 
 import { bookings, customers, rooms} from './testData.js'
-// import Customer from '../src/classes/Customer.js';
-// import Booking from '../src/classes/Booking.js';
+import Customer from '../src/classes/Customer.js';
+import Booking from '../src/classes/Booking.js';
 import Hotel from '../src/classes/Hotel.js';
 
 describe('Hotel', () => {
