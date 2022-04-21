24372009cee40ba089e7c71f7712e562a6a8b201
Rio Foster <friotious@gmail.com>
2022-04-20 17:34:27 -0700

    Add properties and methods to pass test

--- src/classes/Booking.js
+++ src/classes/Booking.js
@@ -1,7 +1,25 @@
 
 class Booking {
-  constructor(data) {
-
+  constructor(bookingData, roomData) {
+    this.id = bookingData.id
+    this.userID = bookingData.userID
+    this.date = bookingData.date
+    this.roomData = roomData
+    this.roomNumber = roomData.number
+    this.roomType = roomData.roomType
+    this.bidet = roomData.bidet
+    this.bedSize = roomData.bedSize
+    this.numBeds = roomData.numBeds
+    this.cost = roomData.costPerNight
   }
+
+  getCost() {
+    return this.cost
+  }
+
+  getDate() {
+    return this.date
+  }
+
 }
 export default Booking;
