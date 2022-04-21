89b035b940725ccf90a070cb84944b0918ed7f70
Rio Foster <friotious@gmail.com>
2022-04-21 13:56:01 -0700

    Remove Booking class of room properties

--- src/classes/Booking.js
+++ src/classes/Booking.js
@@ -1,25 +1,17 @@
-
 class Booking {
   constructor(bookingData, roomData) {
     this.id = bookingData.id
     this.userID = bookingData.userID
     this.date = bookingData.date
     this.roomData = roomData
-    this.roomNumber = roomData.number
-    this.roomType = roomData.roomType
-    this.bidet = roomData.bidet
-    this.bedSize = roomData.bedSize
-    this.numBeds = roomData.numBeds
-    this.cost = roomData.costPerNight
   }
 
   getCost() {
-    return this.cost
+    return this.roomData.costPerNight
   }
 
   getDate() {
     return this.date
   }
-
 }
 export default Booking;
