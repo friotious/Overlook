7ed9e983bcd406326827f28f67c2d5cd336b14e1
Rio Foster <friotious@gmail.com>
2022-04-20 22:43:26 -0700

    Add getCustomerBookings method

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -1,8 +1,35 @@
 
 class Hotel {
-  constructor(bookingData) {
-    this.allBookings = bookingData
+  constructor(bookingData, roomData, customerData, id) {
+    this.bookingData = bookingData
+    this.roomData = roomData
+    this.customerData = customerData
+    this.allBookings = []
+
+    this.currentCustomerId = id
+    this.currentCustomer = {}
+    this.currentCustomerBookings = null
+  }
+
+  updateCurrentCustomer(id, bookings) {
+
+  }
+
+  updateAllBookings() {
     
+  }
+
+  updateCustomerBookings() {
+
+  }
+
+  makeBooking(booking, room) {
+
+  }
+
+  getCustomerBookings(id) {
+    let result = this.allBookings.filter(booking => booking.userID === id)
+    this.currentCustomerBookings = result
   }
 }
 
