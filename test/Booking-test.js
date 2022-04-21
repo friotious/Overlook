a8d79f4bf211dcf3f9a074e730f9966360e9e08d
Rio Foster <friotious@gmail.com>
2022-04-20 18:14:06 -0700

    Modify data to match across testfiles

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -24,30 +24,30 @@ describe('Booking', () => {
     });
 
   it('Should hold Booking data', () => {
-    expect(booking.id).to.equal("5fwrgu4i7k55hl6sz")
-    expect(booking.userID).to.equal(9)
-    expect(booking.date).to.equal("2022/04/22")
-    expect(booking.roomNumber).to.equal(15)
+    expect(booking.id).to.equal("5fwrgu4i7k55hl6t8")
+    expect(booking.userID).to.equal(1)
+    expect(booking.date).to.equal("2022/02/05")
+    expect(booking.roomNumber).to.equal(12)
     expect(booking.cost).to.equal(294.56)
   })
 
   it("Should hold room data for room booked", () => {
     expect(booking.roomData).to.deep.equal(    {
-          number: 15,
-          roomType: "residential suite",
+          number: 12,
+          roomType: "single room",
           bidet: false,
-          bedSize: "full",
-          numBeds: 1,
-          costPerNight: 294.56
+          bedSize: "twin",
+          numBeds: 2,
+          costPerNight: 172.09
         })
   })
 
   it("Should have a method to return booking cost", () => {
-    expect(booking.getCost()).to.equal(294.56)
+    expect(booking.getCost()).to.equal(172.09)
   })
 
   it("Should have a method to return date", () => {
-    expect(booking.getDate()).to.equal("2022/04/22")
+    expect(booking.getDate()).to.equal("2022/02/05")
   })
 
 })
