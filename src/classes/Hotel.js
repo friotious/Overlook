679f7a77b77e636396b4329706a98c4869e3d454
Rio Foster <friotious@gmail.com>
2022-04-22 14:18:45 -0700

    Update methods for Hotel holding Customer-Booking-Room

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -5,67 +5,89 @@ import Customer from "./Customer.js";
 class Hotel {
   constructor(bookingData = [], roomData = [], customerData = [], id) {
     this.currentDate = ''
-    this.bookingData = bookingData
-    this.roomData = roomData
-    this.customerData = customerData
+    //this.bookingData = bookingData
+    //this.roomData = roomData
+    //this.customerData = customerData
     this.allBookings = []
     this.allRooms = []
+    this.allCustomers = []
     this.currentCustomerID = id
+    this.currentCustomer = {}
     this.getCurrentDate()
+    this.updateAllRooms(roomData)
+    this.updateAllBookings(bookingData)
+    this.addRoom()
+    this.updateAllCustomers(customerData)
+    this.addCurrentCustomer(id)
     //this.addCustomer(id)
   }
 
+  updateAllRooms(somedata) {
+    somedata.forEach(room => this.allRooms.push(new Room(room)))
+  }
+
+  updateAllBookings(somedata) {
+    somedata.forEach(booking => this.allBookings.push(new Booking(booking)))
+  }
+
+  updateAllCustomers(somedata) {
+    somedata.forEach(booking => this.allCustomers.push(new Customer(booking)))
+  }
+
+  addCurrentCustomer(id) {
+    this.currentCustomer = this.allCustomers.find(customer => customer.id === id)
+  }
+
+  // addBookings() {
+  //   this.allCustomers.forEach(customer => this.allBookings.forEach(booking => {
+  //     if (booking.userID === customer.id) {
+  //       customer.bookings.push(booking)
+  //     }
+  //   }))
+  // }
+
+  addRoom() {
+    this.allBookings.forEach(booking => this.allRooms.forEach(room => {
+      if (room.number === booking.roomNumber) {
+        booking.room.push(room)
+      }
+    }))
+  }
+
   getCurrentDate() {
-    // ---->   input type date in HTML <------//
+      // ---->   input type date in HTML <------//
     var today = new Date();
-    var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+    var date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate();
     this.currentDate = date
     return date
   }
 
-  // addCustomer(id) {
-  //   this.getCustomerBookings(id)
-  //   let newCustomer = this.customerData.find(customer => customer.id === id)
-  // }
-
-  updateAllBookings() {
-    this.bookingData.forEach(booking => this.allBookings.push(new Booking(booking)))
-  }
-
-  updateAllRooms() {
-    this.roomData.forEach(room => this.allRooms.push(new Room(room)))
-  }
-
-  getBookingByID(id) {
+  getBookingsByID(id) {
     this.currentCustomerID = id
     return this.allBookings.filter(booking => booking.userID === this.currentCustomerID)
   }
-//   let datedbookings = []
-// //declare empty array
-//   let bookedArray = rooms.forEach(room => {
-//     //declare variable method -- take rooms array -> iterate over each room
-//     let result = bookings.filter(booking => booking.roomNumber === room.number)
-//     // result of each action on room element is a variable that is iterating over
-//     // bookings array and filtering out the booking elements that match the room number
-//     return datedbookings.push(result)
-//   })
-  // this.allRooms.forEach(room => {
-  //   let result = this.allBookings.filter(booking => booking.roomNumber === room.number)
-  // })  --> repl --> https://replit.com/@friotious/WrithingSlategreyPipeline#index.js
+    //   let datedbookings = []
+    // //declare empty array
+    //   let bookedArray = rooms.forEach(room => {
+    //     //declare variable method -- take rooms array -> iterate over each room
+    //     let result = bookings.filter(booking => booking.roomNumber === room.number)
+    //     // result of each action on room element is a variable that is iterating over
+    //     // bookings array and filtering out the booking elements that match the room number
+    //     return datedbookings.push(result)
+    //   })
+    // this.allRooms.forEach(room => {
+    //   let result = this.allBookings.filter(booking => booking.roomNumber === room.number)
+    // })  --> repl --> https://replit.com/@friotious/WrithingSlategreyPipeline#index.js
 
-  getCustomerBookings() {
-    let bookings = this.allBookings.filter(booking => booking.userID === this.currentUserID)
-    return bookings
-  }
+    getAllCustomerBookings() {
+      return this.currentCustomer.bookings
+    }
 
-  getCostPerNight(roomNumber) {
-    return this.allRooms.filter(room => room.roomNumber === roomNumber)[0].costPerNight
-  }
 
-  makeBooking(somedata) {
+    makeBooking(somedata) {
+
+    }
 
   }
 
-}
-
-export default Hotel;
+  export default Hotel;
