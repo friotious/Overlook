d1a2d11ddc2b3591f20cbe3299a87ee3abee4bf0
Rio Foster <friotious@gmail.com>
2022-04-22 13:00:06 -0700

    Add autoDateupdate, modify getBooking method

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -11,14 +11,16 @@ class Hotel {
     this.allBookings = []
     this.allRooms = []
     this.currentCustomerID = id
+    this.getCurrentDate()
     //this.addCustomer(id)
   }
 
   getCurrentDate() {
     // ---->   input type date in HTML <------//
     var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
     this.currentDate = date
+    return date
   }
 
   // addCustomer(id) {
@@ -33,6 +35,23 @@ class Hotel {
   updateAllRooms() {
     this.roomData.forEach(room => this.allRooms.push(new Room(room)))
   }
+
+  getBookingByID(id) {
+    this.currentCustomerID = id
+    return this.allBookings.filter(booking => booking.userID === this.currentCustomerID)
+  }
+//   let datedbookings = []
+// //declare empty array
+//   let bookedArray = rooms.forEach(room => {
+//     //declare variable method -- take rooms array -> iterate over each room
+//     let result = bookings.filter(booking => booking.roomNumber === room.number)
+//     // result of each action on room element is a variable that is iterating over
+//     // bookings array and filtering out the booking elements that match the room number
+//     return datedbookings.push(result)
+//   })
+  // this.allRooms.forEach(room => {
+  //   let result = this.allBookings.filter(booking => booking.roomNumber === room.number)
+  // })  --> repl --> https://replit.com/@friotious/WrithingSlategreyPipeline#index.js
 
   getCustomerBookings() {
     let bookings = this.allBookings.filter(booking => booking.userID === this.currentUserID)
