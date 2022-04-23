b1df2e56712d32d8ce53926842cbeb5b5b9dad67
Rio Foster <friotious@gmail.com>
2022-04-23 15:28:13 -0700

    Add structure for importing APIs to scripts.js and apiCalls.js

---
+++ src/apiCalls.js
@@ -0,0 +1,63 @@
+const bookings = fetch("http://localhost:3001/api/v1/bookings")
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw Error(response.statusText)
+    }
+  }).then(data => data)
+  .catch(err => console.log(err))
+
+const customers = fetch("http://localhost:3001/api/v1/customers")
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw Error(response.statusText)
+    }
+  }).then(data => data)
+  .catch(err => console.log(err))
+
+const rooms = fetch("http://localhost:3001/api/v1/rooms")
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw Error(response.statusText)
+    }
+  }).then(data => data)
+  .catch(err => console.log(err))
+
+const bookingPost = (bookingdata) => fetch("http://localhost:3001/api/v1/bookings", {
+method: "POST",
+body: JSON.stringify({
+  'userID': bookingdata.userID,
+  'date': bookingdata.date,
+  'roomNumber': bookingdata.roomNumber
+}),
+headers: {
+  "Content-Type": "application/json"
+}
+}).then(response => {
+  if (response.ok) {
+    return {
+      message: 'Booking with id <id> successfully posted',
+      newBooking: {
+        id: id,
+        userID: bookingdata.userID,
+        date: bookingdata.date,
+        roomNumber: bookingdata.roomNumber
+      }
+    }
+  }
+} else {
+  throw Error(response.statusText)
+}
+}).catch(err => console.log(err))
+
+
+export {
+  bookings,
+  customers,
+  rooms
+}
--- src/scripts.js
+++ src/scripts.js
@@ -3,10 +3,13 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import { bookings, customers, rooms } from './apiCalls.js'
+import Hotel from './classes/Hotel.js';
+import datepicker from 'js-datepicker';
+let moment = require('moment')
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-import datepicker from 'js-datepicker';
 //------
 // make variabel ---
 //const picker = datepicker(input, { alwaysShow: true })
