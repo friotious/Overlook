07c2edd27f117ee8e05ed4bf3674bb8381171a38
Rio Foster <friotious@gmail.com>
2022-04-25 17:56:56 -0700

    Add post bookings method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -33,36 +33,30 @@ const rooms = () => {
   }).then(data => data)
   .catch(err => console.log(err))
 }
-// const bookingPost = (bookingdata) => fetch("http://localhost:3001/api/v1/bookings", {
-// method: "POST",
-// body: JSON.stringify({
-//   'userID': bookingdata.userID,
-//   'date': bookingdata.date,
-//   'roomNumber': bookingdata.roomNumber
-// }),
-// headers: {
-//   "Content-Type": "application/json"
-// }
-// }).then(response => {
-//   if (response.ok) {
-//     return {
-//       message: 'Booking with id <id> successfully posted',
-//       newBooking: {
-//         id: id,
-//         userID: bookingdata.userID,
-//         date: bookingdata.date,
-//         roomNumber: bookingdata.roomNumber
-//       }
-//     }
-//   }
-// } else {
-//   throw Error(response.statusText)
-// }
-// }).catch(err => console.log(err))
+const bookingPost = (id, date, roomNumber) => fetch("http://localhost:3001/api/v1/bookings", {
+      method: "POST",
+      body: JSON.stringify({
+        'userID': id,
+        'date': date,
+        'roomNumber': roomNumber
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+      }).then(response => {
+        if (response.ok) {
+          return response.json()
+          }
+        }
+      } else {
+        throw Error(response.statusText)
+      }
+      }).catch(err => console.log(err))
 
 
 export {
   bookings,
   customers,
-  rooms
+  rooms,
+  bookingPost
 }
