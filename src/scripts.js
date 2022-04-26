e08f1066cc78245b7a106c39be69c6451639c243
Rio Foster <friotious@gmail.com>
2022-04-26 00:10:14 -0700

    Add noVacancy error, showBookings button

--- src/scripts.js
+++ src/scripts.js
@@ -19,10 +19,12 @@ let availRooms = document.getElementById('availableRoomsPage')
 let calendarBox = document.getElementById('calendarBox')
 let calendar = document.getElementById('calendarDate')
 let submitButton = document.getElementById('submitButton')
-let roomInputBox = document.getElementById('roomInputBox')
+let roomDateInputBox = document.getElementById('roomDateInputBox')
 let dateInputError = document.getElementById('dateInputError')
 let roomSelection = document.getElementById("roomTypes")
 let availableRoomsPage = document.getElementById("availableRoomsPage")
+let showBookingsButton = document.getElementById("showBookingsButton")
+let noRoomsError = document.getElementById("noRoomsError")
 let hotel
 let customer
 
@@ -48,20 +50,33 @@ availableRoomsPage.addEventListener('click', (e) => {
   e.target.disabled = true
   let date = moment(calendar.value).format('YYYY/MM/DD')
   if (e.target.type === 'submit') {
-    // console.log(customer.allBookings, 'customer.allBookings before')
     makeBooking(customer.id, e.target.dataset.date, e.target.id)
-    // console.log(e.target.disabled,'before')
-    // console.log(e.target.disabled, 'after')
-    // console.log(customer.allBookings, 'customer.allBookings after')
-    hotel.getAvailRoomsByDate(date)
-    hotel.getAvailRoomsByType(roomSelection.value)
     displayAvailableRooms()
   }
 })
 
-roomInputBox.addEventListener('input', (e) => {
+roomDateInputBox.addEventListener('input', (e) => {
   displayFormInput(e)
 })
+
+showBookingsButton.addEventListener('click', (e) => {
+  console.log('test click me', e)
+  customer.sortBookings()
+  displayCCBookings()
+  displaySpent()
+  resetInputs()
+  show(ccBookings)
+  hide(availRooms)
+  hide(noRoomsError)
+})
+
+const checkVacancy = () => {
+  if(!hotel.availableRooms[0]) {
+    show(noRoomsError)
+    hide(ccBookings)
+    hide(availRooms)
+  }
+}
 
 
 ////<---------> DISPLAY ON PAGE <-------------------------->//////
@@ -116,9 +131,12 @@ const displayFormInput = (e) => {
   }
 }
 
+
+
 //---------- display Room cards ----------//
 
 const displayAvailableRooms = () => {
+  checkVacancy()
   let rooms = hotel.availableRooms
   let date = moment(calendar.value).format('YYYY/MM/DD')
   availRooms.innerHTML = ''
@@ -163,35 +181,11 @@ const show = (element) => element.classList.remove('hidden')
 
 const capitalize = (s) => s[0].toUpperCase() + s.slice(1);
 
+const resetInputs = () => {
+  calendar.value = moment().format('YYYY-MM-DD')
+  roomSelection.value = "Choose a roomtype"
+}
+
 
 
 //------------NOTES - IDEAS - WTF?? ------------------------------------///////////////
-
-//------
-//  const picker = datepicker(input, { alwaysShow: true })
-//  make querySelector for input, next parameter is 'state' of calender
-//  make eventListener for 'click' should return date in some format.
-//----------
-// const newBooking = {
-//   userID: customer.id,
-//   date: calendarDate.value,
-//   roomNumber: e.target.id
-// }
-
-  //.replaceAll("-", "/")
-
-//event.closest()
-
-// const makeBooking = (id, date, roomNumber) => {
-//   Promise.all([bookingPost(id, date, roomNumber)]).then(response => {
-//     console.log(response, 'response')
-//     {
-//     message: 'message',
-//     newBooking: {
-//       id: ';aowkgh;osndv',
-//       date: '22/222/2222',
-//       roomNumber: 22
-//     }
-//     // response[0].newBooking
-//   })
-// }
