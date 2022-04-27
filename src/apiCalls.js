5df78c55291a344a90c332afe087865c5b787466
Rio Foster <friotious@gmail.com>
2022-04-26 18:17:22 -0700

    Refactor apicalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -34,30 +34,6 @@ const rooms = () => {
   .catch(err => console.log(err))
 }
 
-const addActivity = (newActivity) => {
-  fetch("http://localhost:3001/api/v1/activity", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(newActivity),
-  })
-    .then((response) => {
-      console.log(response);
-      if (!response.ok) {
-        throw new Error("Please make sure that all fields are filled out");
-      } else {
-        return response.json();
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      if (error.message === "Failed to fetch") {
-        return (errorTag.innerText = "OOPS SORRY something went wrong");
-      } else {
-        return (errorTag.innerText = error.message);
-      }
-    });
-};
-
 const bookingPost = (id, date, roomNumber) => fetch("http://localhost:3001/api/v1/bookings", {
 method: "POST",
 body: JSON.stringify({
